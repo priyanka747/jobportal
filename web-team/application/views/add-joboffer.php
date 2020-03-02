7b42c5b641c1c1846b2a8e7d7ebe434b4116e142
unknown <thakker747@gmail.com>
2020-03-02 12:57:44 -0500

    login ,signup,add job added

--- web-team/application/views/add-joboffer.php
+++ web-team/application/views/add-joboffer.php
@@ -36,44 +36,40 @@
 					</div>
 					<div class="card-body card-block">
 						<form action="#" method="post" enctype="multipart/form-data" class="form-horizontal">
-							<div class="row form-group">
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label class=" form-control-label">Static</label></div>
 								<div class="col-12 col-md-9">
 									<p class="form-control-static">Username</p>
 								</div>
-							</div>
+							</div> -->
 							<div class="row form-group">
-								<div class="col col-md-3"><label for="text-input" class=" form-control-label">Text Input</label></div>
-								<div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="Text" class="form-control"><small class="form-text text-muted">This is a help text</small></div>
+								<div class="col col-md-3"><label for="text-input" class=" form-control-label">Job Title</label></div>
+								<div class="col-12 col-md-9"><input type="text" id="text-input" name="text-input" placeholder="Job Title" class="form-control"></div>
 							</div>
-							<div class="row form-group">
-								<div class="col col-md-3"><label for="email-input" class=" form-control-label">Email Input</label></div>
-								<div class="col-12 col-md-9"><input type="email" id="email-input" name="email-input" placeholder="Enter Email" class="form-control"><small class="help-block form-text">Please enter your email</small></div>
-							</div>
-							<div class="row form-group">
+							 <div class="row form-group">
+								<div class="col col-md-3"><label for="salary" class=" form-control-label">Salary</label></div>
+								<div class="col-12 col-md-9"><input type="text" id="salary" name="salary" placeholder="$Amount" class="form-control"></div>
+							</div> 
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label for="password-input" class=" form-control-label">Password</label></div>
 								<div class="col-12 col-md-9"><input type="password" id="password-input" name="password-input" placeholder="Password" class="form-control"><small class="help-block form-text">Please enter a complex password</small></div>
-							</div>
+							</div> -->
+							
+							
+
 							<div class="row form-group">
-								<div class="col col-md-3"><label for="disabled-input" class=" form-control-label">Disabled Input</label></div>
-								<div class="col-12 col-md-9"><input type="text" id="disabled-input" name="disabled-input" placeholder="Disabled" disabled="" class="form-control"></div>
-							</div>
-							<div class="row form-group">
-								<div class="col col-md-3"><label for="textarea-input" class=" form-control-label">Textarea</label></div>
-								<div class="col-12 col-md-9"><textarea name="textarea-input" id="textarea-input" rows="9" placeholder="Content..." class="form-control"></textarea></div>
-							</div>
-							<div class="row form-group">
-								<div class="col col-md-3"><label for="select" class=" form-control-label">Select</label></div>
+								<div class="col col-md-3"><label for="select" class=" form-control-label">Job Type</label></div>
 								<div class="col-12 col-md-9">
-									<select name="select" id="select" class="form-control">
-										<option value="0">Please select</option>
-										<option value="1">Option #1</option>
-										<option value="2">Option #2</option>
-										<option value="3">Option #3</option>
+									<select name="Job Type" id="jobtype" class="form-control">
+										<option value="0">Job Type</option>
+										<option value="1">Part-Time</option>
+										<option value="2">Full-Time</option>
 									</select>
 								</div>
 							</div>
-							<div class="row form-group">
+
+							
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label for="selectLg" class=" form-control-label">Select Large</label></div>
 								<div class="col-12 col-md-9">
 									<select name="selectLg" id="selectLg" class="form-control-lg form-control">
@@ -83,49 +79,59 @@
 										<option value="3">Option #3</option>
 									</select>
 								</div>
-							</div>
+							</div> -->
+
+							
+							
 							<div class="row form-group">
-								<div class="col col-md-3"><label for="selectSm" class=" form-control-label">Select Small</label></div>
-								<div class="col-12 col-md-9">
-									<select name="selectSm" id="selectSm" class="form-control-sm form-control">
-										<option value="0">Please select</option>
-										<option value="1">Option #1</option>
-										<option value="2">Option #2</option>
-										<option value="3">Option #3</option>
-										<option value="4">Option #4</option>
-										<option value="5">Option #5</option>
-									</select>
-								</div>
-							</div>
+								<div class="col col-md-3"><label for="skill" class=" form-control-label">Skills Required</label></div>
+								<div class="col-12 col-md-9"><input type="text" id="skill require" name="skillreq" placeholder="Skills" class="form-control"></div>
+							</div> 
+
 							<div class="row form-group">
-								<div class="col col-md-3"><label for="disabledSelect" class=" form-control-label">Disabled Select</label></div>
-								<div class="col-12 col-md-9">
-									<select name="disabledSelect" id="disabledSelect" disabled="" class="form-control">
-										<option value="0">Please select</option>
-										<option value="1">Option #1</option>
-										<option value="2">Option #2</option>
-										<option value="3">Option #3</option>
-									</select>
-								</div>
-							</div>
+								<div class="col col-md-3"><label for="State" class=" form-control-label">State</label></div>
+								<div class="col-12 col-md-9"><input type="text" id="state" name="state" placeholder="state" class="form-control"></div>
+							</div> 
+
+							
+
 							<div class="row form-group">
-								<div class="col col-md-3"><label for="multiple-select" class=" form-control-label">Multiple select</label></div>
+								<div class="col col-md-3"><label for="district" class=" form-control-label">district</label></div>
+								<div class="col-12 col-md-9"><input type="text" id="district" name="district" placeholder="district" class="form-control"></div>
+							</div> 
+
+							
+
+							<div class="row form-group">
+								<div class="col col-md-3"><label class=" form-control-label">Language Required</label></div>
 								<div class="col col-md-9">
-									<select name="multiple-select" id="multiple-select" multiple="" class="form-control">
-										<option value="1">Option #1</option>
-										<option value="2">Option #2</option>
-										<option value="3">Option #3</option>
-										<option value="4">Option #4</option>
-										<option value="5">Option #5</option>
-										<option value="6">Option #6</option>
-										<option value="7">Option #7</option>
-										<option value="8">Option #8</option>
-										<option value="9">Option #9</option>
-										<option value="10">Option #10</option>
-									</select>
+									<div class="form-check">
+										<div class="checkbox">
+											<label for="checkbox1" class="form-check-label ">
+												<input type="checkbox" id="English" name="english" value="english" class="form-check-input">English
+											</label>
+										</div>
+										<div class="checkbox">
+											<label for="checkbox2" class="form-check-label ">
+												<input type="checkbox" id="french" name="french" value="french" class="form-check-input">French
+											</label>
+										</div>
+											</label>
+										</div>
+									</div>
 								</div>
+
+								<div class="row form-group">
+								<div class="col col-md-3"><label for="cmpdetail" class=" form-control-label">Company Detail</label></div>
+								<div class="col-12 col-md-9"><textarea name="cmpdetail" id="textarea-input" rows="9" placeholder="Content..." class="form-control"></textarea></div>
+								</div>
+
 							</div>
-							<div class="row form-group">
+
+							
+						 
+							
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label class=" form-control-label">Radios</label></div>
 								<div class="col col-md-9">
 									<div class="form-check">
@@ -146,8 +152,8 @@
 										</div>
 									</div>
 								</div>
-							</div>
-							<div class="row form-group">
+							</div> -->
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label class=" form-control-label">Inline Radios</label></div>
 								<div class="col col-md-9">
 									<div class="form-check-inline form-check">
@@ -162,30 +168,9 @@
 										</label>
 									</div>
 								</div>
-							</div>
-							<div class="row form-group">
-								<div class="col col-md-3"><label class=" form-control-label">Checkboxes</label></div>
-								<div class="col col-md-9">
-									<div class="form-check">
-										<div class="checkbox">
-											<label for="checkbox1" class="form-check-label ">
-												<input type="checkbox" id="checkbox1" name="checkbox1" value="option1" class="form-check-input">Option 1
-											</label>
-										</div>
-										<div class="checkbox">
-											<label for="checkbox2" class="form-check-label ">
-												<input type="checkbox" id="checkbox2" name="checkbox2" value="option2" class="form-check-input"> Option 2
-											</label>
-										</div>
-										<div class="checkbox">
-											<label for="checkbox3" class="form-check-label ">
-												<input type="checkbox" id="checkbox3" name="checkbox3" value="option3" class="form-check-input"> Option 3
-											</label>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row form-group">
+							</div> -->
+							
+							<!-- <div class="row form-group">
 								<div class="col col-md-3"><label class=" form-control-label">Inline Checkboxes</label></div>
 								<div class="col col-md-9">
 									<div class="form-check-inline form-check">
@@ -208,7 +193,7 @@
 							<div class="row form-group">
 								<div class="col col-md-3"><label for="file-multiple-input" class=" form-control-label">Multiple File input</label></div>
 								<div class="col-12 col-md-9"><input type="file" id="file-multiple-input" name="file-multiple-input" multiple="" class="form-control-file"></div>
-							</div>
+							</div> -->
 						</form>
 					</div>
 					<div class="card-footer">
